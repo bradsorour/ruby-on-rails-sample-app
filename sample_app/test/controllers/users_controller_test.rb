71df39396206dbb323b7bb6603850e5c5c5295ee
Brad Sorour <brad.sorour@gmail.com>
2022-04-22 10:32:41 +0100

    requiring logged in users

--- sample_app/test/controllers/users_controller_test.rb
+++ sample_app/test/controllers/users_controller_test.rb
@@ -1,8 +1,30 @@
 require "test_helper"
 
-class UsersControllerTest < ActionController::TestCase
+class UsersControllerTest < ActionDispatch::IntegrationTest
+
+  def setup
+    @user = users(:brad)
+  end
+
   test "should get new" do
-    get :new
+    get new_user_path
     assert_response :success
   end
+
+  test "should redirect edit when not logged in" do
+    get edit_user_path(@user)
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
+  test "should redirect update when not logged in" do
+    patch user_path(@user), params: { user: { 
+      name: @user.name,
+      email: @user.email 
+      } 
+    }
+    assert_not flash.empty?
+    assert_redirected_to login_url
+  end
+
 end
