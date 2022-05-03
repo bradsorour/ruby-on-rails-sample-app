9f5ffe03228be722177c2f7cef74dec70f2a8eb5
Brad Sorour <brad.sorour@gmail.com>
2022-05-03 13:05:30 +0100

    following tests

--- sample_app/config/application.rb
+++ sample_app/config/application.rb
@@ -19,9 +19,6 @@ module SampleApp
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
-    # Do not swallow errors in after_commit/after_rollback callbacks.
-    config.active_record.raise_in_transactional_callbacks = true
-
     # Include the authenticity token in remote forms.
     config.action_view.embed_authenticity_token_in_remote_forms = true
   end
